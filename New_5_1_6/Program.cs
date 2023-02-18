49eb6d07ff74f657caa8dab104d6ce2c90da1923
Andentu <severalmaz97@gmail.com>
2023-02-18 18:04:11 +0300

    Добавил метод GetArrayFromConsole с массивом result. Добавил обращение к методу GetArrayFromConsole  из метода Main. Добавил получение значений из массива result массивом arr. Добавил сортировку элементов массива и их вывод на экран

--- New_5_1_6/Program.cs
+++ New_5_1_6/Program.cs
@@ -5,6 +5,42 @@
         static void Main(string[] args)
         {
             
+            var arr = GetArrayFromConsole(); // Обращение к методу и массиву его включающему. Также получение массива result в arr
+            int num;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        num = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = num;
+                        Console.WriteLine();
+                        
+                    }
+                }
+            }
+            foreach (var item in arr) 
+            {
+                Console.Write(item + " ");
+            }
+
+
+
         }
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+        
     }
 }
